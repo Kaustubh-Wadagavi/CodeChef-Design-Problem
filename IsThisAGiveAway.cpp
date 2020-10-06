33ca4be739e5915dd3cb5f4c3ed835ae9362ca10
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-10-06 11:35:32 +0530

    IS this a give away problem

---
+++ IsThisAGiveAway.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int a,b,k;
+        cin>>a>>b>>k;
+        if(a==b)
+        cout<<"1"<<endl;
+     else
+        cout<<k<<endl;
+    }
+    
+    return 0;
+}
