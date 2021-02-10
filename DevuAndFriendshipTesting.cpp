3c37463f6ef1a9539c8ec6a6dfc477fb3e3a297f
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-02-10 10:11:21 +0530

    Devu-And-Friendship_testing-problem

---
+++ DevuAndFriendshipTesting.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n; 
+	    int a[n];
+	    int c;
+	    c=1;
+	    for(int i=0;i<n;i++)
+	    {
+	        cin>>a[i];
+	    }
+	   sort(a,a+n);
+	   for(int i = 0 ; i < n-1 ; i++)
+	   {
+	       if(a[i]==a[i+1])
+	       {
+	           continue;
+	       }
+	       else
+	       {
+	           c++;
+	       }
+	   }
+	  cout<<c<<endl;
+	}
+	return 0;
+}
