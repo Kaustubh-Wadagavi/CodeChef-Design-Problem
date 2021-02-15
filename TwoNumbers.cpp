9625ee8e690114491cda800ba4c4139f3c24e044
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-02-15 09:01:56 +0530

    Two-Numbers-Promblem

---
+++ TwoNumbers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int t;
+
+	cin>>t;
+	while(t--)
+	{
+		int a,b,n;
+		cin>>a>>b>>n;
+		if (n%2==0)
+		{
+			cout<<max(a,b)/min(a,b)<<endl;
+		}
+		else
+		{
+			cout<<max(2*a,b)/min(2*a,b)<<endl;
+		}
+	}
+	
+	return 0;
+}
