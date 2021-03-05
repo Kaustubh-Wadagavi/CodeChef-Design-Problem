8acbc5fdcbd7d3308da501e44bd467692e352447
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-03-05 09:32:01 +0530

    ada-king-problem

---
+++ AdaKing.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ar long long int
+using namespace std;
+
+int main()
+{
+  ar t;
+  cin>>t;
+  while(t--)
+ {
+    ar n,m;
+    cin>>n>>m;
+    if(n%2==0||m%2==0)
+    {
+        cout<<"YES"<<endl;
+    }
+    else
+    {
+        cout<<"NO"<<endl;
+    }
+  }
+} 
