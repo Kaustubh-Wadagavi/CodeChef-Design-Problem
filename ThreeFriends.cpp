2b2c4e54d5c9b6c025296f932118339402c608a4
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-04-19 09:12:28 +0530

    Three-Friends-problem

---
+++ ThreeFriends.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if(x== y+z || y==x+z || z ==x+y)
+	    {
+	        cout<<"yes"<<endl;
+	    }
+	    else 
+	    {
+	        cout<<"no"<<endl;
+	    }
+	}
+	return 0;
+}
