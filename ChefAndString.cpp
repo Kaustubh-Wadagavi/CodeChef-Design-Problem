33eae4909f9a5aa4853390a8c1b58d33d5e36a67
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-05-07 09:20:09 +0530

    Chef-and-string-problem

--- ChefAndString.cpp
+++ ChefAndString.cpp
@@ -3,30 +3,40 @@ using namespace std;
 
 int main() 
 {
-  int T; 
-  cin >> T;
-  
-  for (int t = 0; t < T; t++) 
-  {
-    int N; cin >> N;
-    long arr[N];
-    
-    for (int i = 0; i < N; i++) 
-    {
-      long temp; cin >> temp;
-      arr[i] = temp;
-    }
-    
-    long total = 0;
-    
-    for (int i = 0; i < N-1; i++) 
-    {
-        if (arr[i] != arr[i+1]) 
-        {
-            total += abs(arr[i] - arr[i+1]) - 1;
-        }
-    }
-    
-    cout << total << endl;
-  }
+	string s;
+	cin>>s;
+	int a=0,b=0,c=0,d=0;
+	for(int i=0;i<s.length();i++)
+	{
+	    if(s[i]=='C')
+	    {
+	        a++;
+	    }
+	    else if(s[i]=='H')
+	    {
+	        if(a>0)
+	        {
+	            a--;
+	            b++;
+	        }
+	    }
+	    else if(s[i]=='E')
+	    {
+	        if(b>0)
+	        {
+	            b--;
+	            c++;
+	        }
+	    }
+	    else if(s[i]=='F')
+	    {
+	        if(c>0)
+	        {
+	            c--;
+	            d++;
+	        }
+	    }
+	}
+	cout<<d<<endl;
+	return 0;
 }
