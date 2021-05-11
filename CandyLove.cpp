716b3e809e2c4079c2be413a70399bb8e88dbafb
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2021-05-11 09:21:30 +0530

    Candy-Love-Problem

---
+++ CandyLove.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, i, sum = 0;
+        cin >> n;
+        int a[n];
+        for (i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+        for (i = 0; i < n; i++)
+        {
+            sum = sum + a[i];
+        }
+        if (sum % 2 == 1)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
